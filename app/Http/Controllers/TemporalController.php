44b1775f579eef442b52128a9b7646bbcfc79213
Camilo <camilo.callejas@icloud.com>
2024-04-15 19:27:21 -0500

    Inicialización

--- app/Http/Controllers/TemporalController.php
+++ app/Http/Controllers/TemporalController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TemporalController extends Controller
 {
-    //
+    public function index() {
+        //Iniciar sesión
+        Auth::loginUsingId(1);
+        //Iniciar sesión
+    }
+
+    public function logout() {
+        //Cerrar sesión
+        Auth::logout();
+        //Cerrar sesión
+    }
 }
