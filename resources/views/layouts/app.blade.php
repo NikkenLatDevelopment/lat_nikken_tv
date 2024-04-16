c9c41f5999de7c0b3483adf8f628e77ba6725f5f
Camilo <camilo.callejas@icloud.com>
2024-04-15 19:10:26 -0500

    Inicialización

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+        <title>@yield('title') - Mi Tienda NIKKEN</title>
+        @stack('SEO')
+
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('assets/img/favicon/site.webmanifest') }}">
+        <link rel="mask-icon" href="{{ asset('assets/img/favicon/safari-pinned-tab.svg') }}" color="#00aba9">
+        <link rel="shortcut icon" href="{{ asset('assets/img/favicon/favicon.ico') }}">
+        <meta name="msapplication-TileColor" content="#00aba9">
+        <meta name="msapplication-config" content="{{ asset('assets/img/favicon/browserconfig.xml') }}">
+        <meta name="theme-color" content="#ffffff">
+
+        @stack('styles')
+    </head>
+
+    <body>
+        @yield('content')
+
+        @vite([ 'resources/js/app.js' ])
+        @stack('scripts')
+    </body>
+</html>
