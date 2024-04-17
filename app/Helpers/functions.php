b3068e0c8fb971425867eb5be841762abfc9c9b4
Camilo <camilo.callejas@icloud.com>
2024-04-16 19:01:09 -0500

    Inicialización

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Carbon;
 
 function formatPriceWithCurrency(float $price, array $country) {
-    //Formatear precio
+    //Formatear precio con símbolo de moneda
     return $country['currency_symbol'] . number_format($price, $country['currency_decimal']);
 }
 
 function formatDateInSpanishLocale(string $date) {
-    //Formatear fecha a texto
+    //Formatear fecha a texto en español
     return Carbon::parse($date)->locale('es_ES')->isoFormat('DD [de] MMMM [del] YYYY');
 }
