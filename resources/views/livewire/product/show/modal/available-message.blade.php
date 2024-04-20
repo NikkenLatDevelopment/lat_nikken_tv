3e63654fc6ec7d93199b156ebe1feba363f4bf95
Camilo <camilo.callejas@icloud.com>
2024-04-20 13:39:51 -0500

    Inicialización

--- resources/views/livewire/product/show/modal/available-message.blade.php
+++ resources/views/livewire/product/show/modal/available-message.blade.php
@@ -8,9 +8,9 @@
 
                         <ul class="fa-ul ps-0 ms-4 components">
                             @foreach ($componentsNotAvailable as $componentNotAvailable)
-                                <li class="mb-2">
+                                <li class="mb-3">
                                     <span class="fa-li"><i class="fa-solid fa-angle-right fa-xs"></i></span>
-                                    <span class="lh-sm d-block">{{ $componentNotAvailable['sku'] }} - <span class="fw-bold">{{ $componentNotAvailable['name'] }}</span>.</span>
+                                    <span class="lh-sm d-block mb-2">{{ $componentNotAvailable['sku'] }} - <span class="fw-bold">{{ $componentNotAvailable['name'] }}</span>.</span>
                                     <span class="small lh-sm d-block">Fecha estimada de disponibilidad:</span>
                                     <span class="lh-sm d-block"><i class="fi fi-sr-calendar-clock position-relative me-1"></i> <span class="fs-6 fw-bold text-decoration-underline">{{ $componentNotAvailable['date'] }}</span>.</span>
                                 </li>
@@ -22,9 +22,9 @@
                         <p class="text-center lh-sm mt-2">Recuerda que este producto <span class="fs-5 text-warning text-decoration-underline lh-1 custom fw-extrabold">no se encuentran disponible:</span></p>
 
                         <ul class="fa-ul ps-0 ms-4 components">
-                            <li class="mb-2">
+                            <li class="mb-3">
                                 <span class="fa-li"><i class="fa-solid fa-angle-right fa-xs"></i></span>
-                                <span class="lh-sm d-block">{{ $sku }} - <span class="fw-bold">{{ $name }}</span>.</span>
+                                <span class="lh-sm d-block mb-2">{{ $sku }} - <span class="fw-bold">{{ $name }}</span>.</span>
                                 <span class="small lh-sm d-block">Fecha estimada de disponibilidad:</span>
                                 <span class="lh-sm d-block"><i class="fi fi-sr-calendar-clock position-relative me-1"></i> <span class="fs-6 fw-bold text-decoration-underline">{{ $availableUntil }}</span>.</span>
                             </li>
