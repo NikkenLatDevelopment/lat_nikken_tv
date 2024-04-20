46428acf0eef613c5206d043530afadecf6c752e
Camilo <camilo.callejas@icloud.com>
2024-04-20 13:44:35 -0500

    Inicialización

--- resources/views/components/general/header/content/cart.blade.php
+++ resources/views/components/general/header/content/cart.blade.php
@@ -1,10 +1,10 @@
-<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasCart" aria-labelledby="offcanvasCartLabel">
-    <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasCartLabel">Offcanvas</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+<div class="offcanvas offcanvas-end border-0 cart" tabindex="-1" id="offcanvasCart" aria-labelledby="offcanvasCartLabel">
+    <div class="offcanvas-header shadow-sm p-4">
+        <div class="offcanvas-title h5 text-dark fw-bold lh-1" id="offcanvasCartLabel">Carrito de Compras</div>
+        <button type="button" class="btn-close me-1" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
 
-    <div class="offcanvas-body">
+    <div class="offcanvas-body bg-light p-3">
         @livewire('general.header.content.cart.products')
     </div>
 </div>
--- resources/views/livewire/general/header/content/cart/products.blade.php
+++ resources/views/livewire/general/header/content/cart/products.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- Be like water. --}}
+    <div class="products">
+        @forelse ($products as $index => $product)
+        @empty
+            <div class="text-center d-flex align-items-center justify-content-center empty">
+                <div>
+                    <img src="{{ asset('assets/img/general/empty-product.png') }}" class="img-fluid" alt="Lista de Deseos">
+                    <p class="h5 text-black-50 opacity-50 fw-bold mt-3 mb-0">Tu lista de deseos está vacia.</p>
+                    <p class="small text-black-50 opacity-50 fw-semibold">Empieza agregrando un producto.</p>
+                </div>
+            </div>
+        @endforelse
+    </div>
 </div>
