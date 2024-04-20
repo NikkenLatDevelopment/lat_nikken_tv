805fea58866a1707f80c3182a7a9c4a77d8e3e84
Camilo <camilo.callejas@icloud.com>
2024-04-20 17:27:56 -0500

    Inicialización

--- database/factories/CartProductFactory.php
+++ database/factories/CartProductFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CartItem>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CartProduct>
  */
-class CartItemFactory extends Factory
+class CartProductFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/migrations/2024_04_20_162401_create_cart_products_table.php
+++ database/migrations/2024_04_20_162401_create_cart_products_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cart_items', function (Blueprint $table) {
+        Schema::create('cart_products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('cart_id')->constrained();
             $table->foreignId('product_id')->constrained();
-            $table->integer('quantity')->default(1);
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cart_items');
+        Schema::dropIfExists('cart_products');
     }
 };
