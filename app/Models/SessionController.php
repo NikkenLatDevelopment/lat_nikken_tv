64f66dcbde3676a2cfcd921c511339508fecbd3e
Camilo <camilo.callejas@icloud.com>
2024-04-20 23:05:52 -0500

    Inicialización

--- app/Models/SessionController.php
+++ app/Models/SessionController.php
@@ -26,7 +26,7 @@ class SessionController
         Cookie::queue('country_id', $country->id);
     }
 
-    public function getCountry(): ?object {
+    public function getCountry(): ?SessionControllerCountry {
         //Obtener información
         $country = $this->session->get('country');
 
@@ -35,24 +35,15 @@ class SessionController
     }
 
     public function getCountryId(): ?int {
-        //Obtener ID del país por sesión
-        $countryId = $this->session->get('country.id');
-
-        if (empty($countryId)) {
-            //Obtener ID del país por cookie
-            $countryId = Cookie::get('country_id');
-        }
-
-        return $countryId;
+        //Obtener ID del país por sesión o cookie
+        return $this->session->get('country.id') ?? Cookie::get('country_id');;
     }
 
     public function setCart(int $productId, int $quantity): void {
         //Obtener información del usuario
-        $user = Auth::user();
-
-        if ($user) {
+        if (Auth::user()) {
             //Guardar producto en carrito de compras
-            $user->cart()->updateOrCreate(
+            Auth::user()->cart()->updateOrCreate(
                 [ 'catalog_country_id' => $this->session->get('country.id'), 'product_id' => $productId ],
                 [ 'quantity' => $quantity ]
             );
@@ -72,19 +63,7 @@ class SessionController
 
                 if ($product) {
                     //Agregar producto al carrito de compras
-                    $cart[] = [
-                        'id' => $product->id,
-                        'slug' => $product->slug,
-                        'sku' => $product->sku,
-                        'name' => $product->name,
-                        'image' => env('STORAGE_PRODUCT_IMAGE_MAIN_PATH') . $product->image,
-                        'price' => formatPriceWithCurrency($product->suggested_price, $this->session->get('country')),
-                        'quantity' => $quantity,
-                        'total' => formatPriceWithCurrency($product->suggested_price * $quantity, $this->session->get('country')),
-                        'available' => array_values($product->getAvailability())[0],
-                        'rating' => $product->rating_total,
-                        'brandSlug' => $product->catalogProductBrand->slug
-                    ];
+                    $cart[] = formatCartProduct($product, $quantity, $this->session->get('country'));
                 }
             }
 
@@ -95,35 +74,19 @@ class SessionController
 
     public function getCart(): array {
         //Obtener información del usuario
-        $user = Auth::user();
-
-        if ($user) {
+        if (Auth::user()) {
             //Obtener carrito de compras de base de datos
-            return $user->cart()
+            return Auth::user()->cart
             ->with('product', 'product.catalogProductBrand')
             ->whereHas('product', fn($query) => $query->active($this->session->get('country.id')))
             ->country($this->session->get('country.id'))
             ->get()
-            ->map(function($cart) {
-                return [
-                    'id' => $cart->product->id,
-                    'slug' => $cart->product->slug,
-                    'sku' => $cart->product->sku,
-                    'name' => $cart->product->name,
-                    'image' => env('STORAGE_PRODUCT_IMAGE_MAIN_PATH') . $cart->product->image,
-                    'price' => formatPriceWithCurrency($cart->product->suggested_price, $this->session->get('country')),
-                    'quantity' => $cart->quantity,
-                    'total' => formatPriceWithCurrency($cart->product->suggested_price * $cart->quantity, $this->session->get('country')),
-                    'available' => array_values($cart->product->getAvailability())[0],
-                    'rating' => $cart->product->rating_total,
-                    'brandSlug' => $cart->product->catalogProductBrand->slug
-                ];
-            })
+            ->map(function($cart) { return formatCartProduct($cart->product, $cart->quantity, $this->session->get('country')); })
             ->toArray();
-        } else {
-            //Obtener carrito de compras de sesión
-            return $this->session->get('cart', []);
         }
+
+        //Obtener carrito de compras de sesión
+        return $this->session->get('cart', []);
     }
 
     public function removeCart(int $productId): void {
@@ -166,4 +129,20 @@ class SessionControllerCountry {
         //Generar array
         return [ 'id' => $this->id, 'code' => $this->code, 'name' => $this->name, 'vat' => $this->vat, 'currency_symbol' => $this->currency_symbol, 'currency_decimal' => $this->currency_decimal ];
     }
+}
+
+function formatCartProduct(Product $product, int $quantity, array $country): array {
+    return [
+        'id' => $product->id,
+        'slug' => $product->slug,
+        'sku' => $product->sku,
+        'name' => $product->name,
+        'image' => env('STORAGE_PRODUCT_IMAGE_MAIN_PATH') . $product->image,
+        'price' => formatPriceWithCurrency($product->suggested_price, $country),
+        'quantity' => $quantity,
+        'total' => formatPriceWithCurrency($product->suggested_price * $quantity, $country),
+        'available' => array_values($product->getAvailability())[0],
+        'rating' => $product->rating_total,
+        'brandSlug' => $product->catalogProductBrand->slug,
+    ];
 }
