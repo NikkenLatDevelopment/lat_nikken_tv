d8ee9b64b0d8075a7932e48359551f5d1a2052a0
Camilo <camilo.callejas@icloud.com>
2024-04-21 22:28:45 -0500

    Inicialización

--- app/Models/CatalogProductBrand.php
+++ app/Models/CatalogProductBrand.php
@@ -9,14 +9,14 @@ class CatalogProductBrand extends Model
 {
     use HasFactory;
 
+    public function products() {
+        //Relación con los productos
+        return $this->hasMany(Product::class);
+    }
+
     public function scopeStatus($query, ?int $view = 0) {
         //Filtrar por estatus y omitir sin marca
         return $query->when($view == 0, fn ($query) => $query->where('id', '>', 1))
                      ->where('status', 1);
-    }
-
-    public function products() {
-        //Relación con los productos
-        return $this->hasMany(Product::class);
     }
 }
