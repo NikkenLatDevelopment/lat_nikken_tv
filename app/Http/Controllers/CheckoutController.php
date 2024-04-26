1acba76b64c8abc7029138f8cb4d9a561acaf0ae
Camilo <camilo.callejas@icloud.com>
2024-04-26 16:25:49 -0500

    Inicialización

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class CheckoutController extends Controller
 {
     public function index() {
+        if (!auth()->check()) {
+            //Redireccionar
+            return redirect()->route('login');
+        }
+
         //Mostrar vista
         return view('checkout.index');
     }
