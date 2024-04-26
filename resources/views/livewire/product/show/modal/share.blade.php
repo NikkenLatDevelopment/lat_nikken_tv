9bafb528b03c11d303201e80107e002849dfabeb
Camilo <camilo.callejas@icloud.com>
2024-04-26 15:38:10 -0500

    Inicialización

--- resources/views/livewire/product/show/modal/share.blade.php
+++ resources/views/livewire/product/show/modal/share.blade.php
@@ -1,14 +1,14 @@
 <div>
     <div class="modal fade" id="productShowModalShare" tabindex="-1">
         <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content border border-success border-4 rounded-4 overflow-hidden">
+            <div class="modal-content border border-dark border-4 rounded-4 overflow-hidden">
                 <div class="modal-body p-4">
                     <div class="position-absolute top-0 end-0 p-2"><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button></div>
 
                     <div class="h5 text-dark fw-bold mb-0"><i class="fi fi-br-share-square position-relative mb-0 custom i-top-4"></i> Compartir</div>
                     <p class="lh-sm">Comparte este link con tus amigo(a)s y conocido(a)s.</p>
 
-                    <div class="input-group rounded-3 shadow-sm overflow-hidden mb-3">
+                    <div class="input-group rounded-3 overflow-hidden mb-3">
                         <div class="form-floating">
                             <input type="text" class="form-control text-dark fw-bold text-decoration-underline bg-light border-end-0 border-light shadow-none" id="product-share-url" placeholder="Link" value="{{ $currentUrl }}">
                             <label for="product-share-url">Link</label>
