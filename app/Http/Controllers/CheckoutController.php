f0978dc183486937b4cebb912a38befd91f99bb2
Camilo <camilo.callejas@icloud.com>
2024-04-27 19:55:40 -0500

    Inicialización

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -7,25 +7,10 @@ use App\Models\SessionController;
 
 class CheckoutController extends Controller
 {
-    protected SessionController $sessionController;
-
-    public function __construct(SessionController $sessionController) {
-        //Iniciar sesión
-        $this->sessionController = $sessionController;
-    }
-
     public function index() {
         if (!auth()->check()) {
             //Redireccionar
             return redirect()->route('login');
-        }
-
-        //Obtener productos del carrito de compras
-        $products = $this->sessionController->getCart();
-
-        if (empty($products)) {
-            //Redireccionar
-            return redirect()->route('home');
         }
 
         //Mostrar vista
