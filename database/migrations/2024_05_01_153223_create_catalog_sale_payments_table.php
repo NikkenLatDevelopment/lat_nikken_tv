09689822683e5eb643c041f62024f74751821643
Camilo <camilo.callejas@icloud.com>
2024-05-01 16:33:42 -0500

    Inicialización

---
+++ database/migrations/2024_05_01_153223_create_catalog_sale_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('catalog_sale_payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('catalog_country_id')->constrained();
+            $table->string('name', 60)->index();
+            $table->tinyInteger('status')->index()->default(1);
+            $table->timestamps();
+
+            //No permitir formas de pago duplicadas
+            $table->unique([ 'catalog_country_id', 'name' ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('catalog_sale_payments');
+    }
+};
