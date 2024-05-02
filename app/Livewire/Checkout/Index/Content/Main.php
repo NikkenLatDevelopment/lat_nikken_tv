2772b3ca003e507be8d599010bcc26c34b474d65
Camilo <camilo.callejas@icloud.com>
2024-05-02 00:43:31 -0500

    Inicialización

--- app/Livewire/Checkout/Index/Content/Main.php
+++ app/Livewire/Checkout/Index/Content/Main.php
@@ -7,20 +7,24 @@ use Livewire\Attributes\On;
 use Livewire\Attributes\Locked;
 use App\Livewire\Forms\CartForm;
 use App\Models\SessionController;
-use App\Livewire\Forms\AddressForm;
-use Illuminate\Support\Facades\Validator;
+use App\Livewire\Forms\UserAddressForm;
+use App\Models\CatalogSalePaymentMethod;
 
 class Main extends Component
 {
-    public CartForm $cartForm;
-    public AddressForm $addressForm;
+    #[Locked]
+    public array $catalogSalePaymentMethods = [];
 
     public bool $discountSuggestedPrice = false;
+    public int $selectedCatalogSalePaymentMethod;
 
-    public string $state;
-    public string $municipality;
-    public string $colony;
-    public int $postalCode;
+    public CartForm $cartForm;
+    public UserAddressForm $userAddressForm;
+
+    public string $stateUserAddressForm;
+    public string $municipalityUserAddressForm;
+    public string $colonyUserAddressForm;
+    public int $postalCodeUserAddressForm;
 
     public function render()
     {
@@ -30,35 +34,48 @@ class Main extends Component
 
     public function mount(SessionController $sessionController) {
         //Obtener información del país
-        $country = $sessionController->getCountry()->toArray();
+        $catalogCountry = $sessionController->getCatalogCountry()->toArray();
 
         //Obtener información del sugerido con descuento
         $this->discountSuggestedPrice = $sessionController->getDiscountSuggestedPrice();
 
-        //Iniciar formulario carrito de compras
-        $this->cartForm->country = $country;
+        //Inicializar formulario carrito de compras
+        $this->initializeCartForm($catalogCountry, $sessionController);
+
+        //Inicializar formulario direcciones del usuario
+        $this->initializeUserAddressForm($catalogCountry);
+
+        //Obtener catálogo de formas de pago de la venta
+        $this->getCatalogSalePaymentMethods($catalogCountry);
+    }
+
+    public function initializeCartForm(array $catalogCountry, SessionController $sessionController) {
+        //Inicializar información
+        $this->cartForm->catalogCountry = $catalogCountry;
         $this->cartForm->discountSuggestedPrice = $this->discountSuggestedPrice;
 
         //Obtener productos del carrito de compras
         $this->getProducts($sessionController);
+    }
 
-        //Iniciar formulario dirección
-        $this->addressForm->country = $country;
+    public function initializeUserAddressForm(array $catalogCountry) {
+        //Inicializar información
+        $this->userAddressForm->catalogCountry = $catalogCountry;
 
-        if ($this->addressForm->country['id'] != 2) {
+        if ($this->userAddressForm->catalogCountry['id'] != 2) {
             //Obtener catálogo de estados
-            $this->addressForm->getCatalogStates();
+            $this->userAddressForm->getApiCatalogStates();
         }
 
-        //Obtener la cantidad de direcciones registradas
-        $this->addressForm->getTotalAddresses();
+        //Obtener la cantidad de direcciones registradas por el usuario
+        $this->userAddressForm->getCountUserAddresses();
     }
 
     public function getProducts(SessionController $sessionController) {
         //Obtener productos del carrito de compras
         $this->cartForm->getProducts($sessionController);
 
-        //Calcular totales carrito de compras
+        //Generar los cálculos del carrito de compras
         $this->getTotals();
     }
 
@@ -68,52 +85,52 @@ class Main extends Component
             return redirect()->route('home');
         }
 
-        //Calcular totales carrito de compras
+        //Generar los cálculos del carrito de compras
         $this->cartForm->getTotals();
     }
 
     #[On('checkout.index.content.main.removeProduct')]
-    public function removeProduct(int $productId, SessionController $sessionController) {
+    public function removeProduct(int $id, SessionController $sessionController) {
         //Eliminar producto del carrito de compras
-        $validate = $this->cartForm->removeProduct($productId, true, $sessionController);
+        $validate = $this->cartForm->removeProduct($id, true, $sessionController);
 
         if ($validate) {
             //Emitir evento para mostrar mensaje de confirmación
             $this->dispatch('showToast', message: 'Producto <span class="fw-bold"><u>eliminado</u></span> de tu carrito de compras.', color: 'dark');
 
-            //Emitir evento para eliminar el producto del carrito de compras del menú
-            $this->dispatch('general.header.content.cart.products.removeProductExternal', productId: $productId);
+            //Emitir evento para eliminar el producto en el carrito de compras del menú
+            $this->dispatch('general.header.content.cart.products.removeProductExternal', id: $id);
 
-            //Calcular totales carrito de compras
+            //Generar los cálculos del carrito de compras
             $this->getTotals();
         }
     }
 
     #[On('checkout.index.content.main.removeProductExternal')]
-    public function removeProductExternal(int $productId, SessionController $sessionController) {
+    public function removeProductExternal(int $id, SessionController $sessionController) {
         //Eliminar producto del carrito de compras
-        $validate = $this->cartForm->removeProduct($productId, false, $sessionController);
+        $validate = $this->cartForm->removeProduct($id, false, $sessionController);
 
         if ($validate) {
-            //Calcular totales carrito de compras
+            //Generar los cálculos del carrito de compras
             $this->getTotals();
         }
     }
 
     #[On('checkout.index.content.main.changeQuantity')]
-    public function changeQuantity(int $productId, int $quantity, SessionController $sessionController) {
+    public function changeQuantity(int $id, int $quantity, SessionController $sessionController) {
         //Actualizar cantidad del producto en el carrito de compras
-        list($product, $validate) = $this->cartForm->changeQuantity($productId, $quantity, true, $sessionController);
+        list($product, $validate) = $this->cartForm->changeQuantity($id, $quantity, true, $sessionController);
 
         //Emitir evento para actualizar el producto en el carrito de compras
-        $this->dispatch('checkout.index.content.resumeProduct.refreshProduct.' . $productId, product: $product);
+        $this->dispatch('checkout.index.content.resumeProduct.refreshProduct.' . $id, product: $product);
 
         if ($validate) {
-            //Calcular totales carrito de compras
+            //Generar los cálculos del carrito de compras
             $this->getTotals();
 
             //Emitir evento para cambiar la cantidad del producto en el carrito de compras del menú
-            $this->dispatch('general.header.content.cart.products.changeQuantity', productId: $productId, quantity: $quantity);
+            $this->dispatch('general.header.content.cart.products.changeQuantity', id: $id, quantity: $quantity);
         }
     }
 
@@ -121,10 +138,10 @@ class Main extends Component
         //Actualizar sugerido con descuento
         $this->discountSuggestedPrice = $this->cartForm->changeDiscountSuggestedPrice($this->discountSuggestedPrice, true, $sessionController);
 
-        //Calcular totales carrito de compras
+        //Generar los cálculos del carrito de compras
         $this->getTotals();
 
-        //Emitir evento para actualizar el sugerido con descuento en el menú
+        //Emitir evento para actualizar el sugerido con descuento del menú
         $this->dispatch('general.header.content.cart.products.updatedDiscountSuggestedPriceExternal', discountSuggestedPrice: $this->discountSuggestedPrice);
     }
 
@@ -133,72 +150,96 @@ class Main extends Component
         //Actualizar sugerido con descuento
         $this->discountSuggestedPrice = $this->cartForm->changeDiscountSuggestedPrice($discountSuggestedPrice, false, $sessionController);
 
-        //Calcular totales carrito de compras
+        //Generar los cálculos del carrito de compras
         $this->getTotals();
     }
 
-    public function updatedState() {
+    public function updatedStateUserAddressForm() {
         //Validar información
-        $this->validate([ 'state' => 'required|string|max:40' ]);
+        $this->validate([ 'stateUserAddressForm' => 'required|string|max:255' ], [], [ 'stateUserAddressForm' => 'estado' ]);
 
-        //Limpiar municipio
-        $this->municipality = '';
+        //Actualizar estado
+        $this->userAddressForm->state = $this->stateUserAddressForm;
+
+        //Limpiar información
+        $this->municipalityUserAddressForm = '';
+        $this->colonyUserAddressForm = '';
+        $this->userAddressForm->municipality = '';
+        $this->userAddressForm->colony = '';
+        $this->userAddressForm->catalogMunicipalities = [];
+        $this->userAddressForm->catalogColonies = [];
 
         //Obtener catálogo de municipios
-        $this->addressForm->getCatalogMunicipalities($this->state);
+        $this->userAddressForm->getApiCatalogMunicipalities();
     }
 
-    public function updatedMunicipality() {
-        //Validar si el país aplica para obtener las colonias
-        if (in_array($this->addressForm->country['id'], [ 3, 4, 5, 8, 10 ])) {
+    public function updatedMunicipalityUserAddressForm() {
+        //Actualizar municipio
+        $this->userAddressForm->municipality = $this->municipality;
+
+        //Limpiar información
+        $this->colonyUserAddressForm = '';
+        $this->userAddressForm->colony = '';
+        $this->userAddressForm->catalogColonies = [];
+
+        if (in_array($this->userAddressForm->catalogCountry['id'], [ 3, 4, 5, 8, 10 ])) {
             //Validar información
             $this->validate([
-                'state' => 'required|string|max:40',
-                'municipality' => 'required|string|max:40'
+                'stateUserAddressForm' => 'required|string|max:255',
+                'municipalityUserAddressForm' => 'required|string|max:255'
+            ], [], [
+                'stateUserAddressForm' => 'estado',
+                'municipalityUserAddressForm' => 'municipio'
             ]);
 
-            //Limpiar colonia
-            $this->colony = '';
-
             //Obtener catálogo de colonias
-            $this->addressForm->getCatalogColonies($this->state, $this->municipality);
+            $this->userAddressForm->getApiCatalogColonies();
         }
     }
 
-    public function updatedPostalCode() {
-        //Validar si el país aplica para obtener el código postal
-        if ($this->addressForm->country['id'] == 2) {
+    public function updatedColonyUserAddressForm() {
+        //Validar información
+        $this->validate([ 'colonyUserAddressForm' => 'required|string|max:255' ], [], [ 'colonyUserAddressForm' => 'colonia' ]);
+
+        //Actualizar colonia
+        $this->userAddressForm->colony = $this->colonyUserAddressForm;
+    }
+
+    public function updatedPostalCodeUserAddressForm() {
+        if ($this->userAddressForm->catalogCountry['id'] == 2) {
             //Validar información
-            $this->validate([ 'postalCode' => 'required|max:5|min:5' ], [], [ 'postalCode' => 'código postal' ]);
+            $this->validate([ 'postalCodeUserAddressForm' => 'required|string|max:5|min:5' ], [], [ 'postalCodeUserAddressForm' => 'código postal' ]);
 
-            //Limpiar estado
-            $this->state = '';
+            //Actualizar código postal
+            $this->userAddressForm->postalCode = $this->postalCodeUserAddressForm;
 
-            //Limpiar municipio
-            $this->municipality = '';
-
-            //Limpiar colonia
-            $this->colony = '';
+            //Limpiar información
+            $this->stateUserAddressForm = '';
+            $this->municipalityUserAddressForm = '';
+            $this->colonyUserAddressForm = '';
+            $this->userAddressForm->state = '';
+            $this->userAddressForm->municipality = '';
+            $this->userAddressForm->colony = '';
+            $this->userAddressForm->catalogStates = [];
+            $this->userAddressForm->catalogMunicipalities = [];
+            $this->userAddressForm->catalogColonies = [];
 
             //Obtener catálogo de estados, municipios y colonias
-            $this->addressForm->getCatalogMex($this->postalCode);
+            $this->userAddressForm->getApiCatalogs();
         }
     }
 
-    #[On('checkout.index.content.main.selectedAddressExternal')]
-    public function selectedAddressExternal(int $addressId) {
-        //Validar información
-        Validator::make(
-            [ 'addressId' => $addressId ],
-            [ 'addressId' => 'required|integer|exists:user_addresses,id' ]
-        )->validate();
-
-        //Guardar dirección seleccionada
-        $this->addressForm->selectedAddress = $addressId;
+    #[On('checkout.index.content.main.changeSelectedUserAddressExternal')]
+    public function changeSelectedUserAddressExternal(int $id) {
+        //Guardar dirección seleccionada por el usuario
+        $this->userAddressForm->selectedUserAddress = $id;
     }
 
-    public function changeTypeSelectedAddress(int $typeSelectedAddress) {
-        //Cambiar tipo de dirección seleccionada
-        $this->addressForm->typeSelectedAddress = $typeSelectedAddress;
+    public function getCatalogSalePaymentMethods() {
+        //Obtener catálogo de formas de pago de la venta
+        $this->catalogSalePaymentMethods = CatalogSalePaymentMethod::catalogCountryId($this->catalogCountry['id'])
+        ->status()
+        ->get()
+        ->toArray();
     }
 }
